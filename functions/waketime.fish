dd54738af61dba7499f8486273263cf47ee5187a
ik-fib <ik11235@gmail.com>
2022-12-24 19:17:31 +0900

    Defined as a function, preexec support, to work with wakatime-cli

--- functions/waketime.fish
+++ functions/waketime.fish
@@ -1,9 +1,18 @@
-set -l project
+function register_wakatime_fish_before_exec -e fish_preexec
+  set -l project
+  set -l wakatime_path (type -p wakatime)
+  if test -z "$wakatime_path"
+     set wakatime_path (type -p ~/.wakatime/wakatime-cli)
+     if test -z "$wakatime_path"
+       echo "wakatime command not found. Please read "https://wakatime.com/terminal" and install wakatime."
+       return 1
+     end
+  end
 
-if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
-    set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
-else
-    set  project "Terminal"
+  if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
+      set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
+  else
+      set  project "Terminal"
+  end
+  $wakatime_path --write --plugin "ik11235/waketime.fish/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
 end
-
-wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
